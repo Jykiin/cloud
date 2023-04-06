928a26c61285d620c8e6cea0660b32e239de830d
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-06 09:33:46 +0200

    script

--- app/script/create-user.php
+++ app/script/create-user.php
@@ -3,14 +3,6 @@
 $username = $_GET['username'];
 $password = $_GET['password'];
 
-# Créer le compte utilisateur
-shell_exec("sudo useradd –m  $username");
+shell_exec("./createuser.sh $username $password");
 
-# Définir le mot de passe pour le compte utilisateur
-shell_exec("sudo echo '$username:$password' | sudo chpasswd");
-
-# Ajouter le compte utilisateur au groupe sudo pour accorder les privilèges de superutilisateur
-#shell_exec("usermod -aG sudo $username");
-
-# Afficher un message de confirmation
-echo "<h1 style='color: green;'>Le compte utilisateur <strong style='color: black'>$username</strong> a été créé avec succès. </h1>";
+echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
---
+++ app/script/createuser.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+"sudo useradd –m $1";
+"sudo echo '$1:$2' | sudo chpasswd";
