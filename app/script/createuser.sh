6dc0105154ca88730587c719f5751b8c3ffc0506
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-06 16:01:22 +0200

    scrip for edited site added

--- app/script/createuser.sh
+++ app/script/createuser.sh
@@ -3,3 +3,36 @@
 sudo useradd -m $1
 sudo echo $1:$2 | sudo chpasswd
 sudo mv authorized_keys /home/$1/.ssh/
+
+
+# Vérifiez si le nom du site est fourni
+if [ $# -eq 0 ]; then
+    echo "Veuillez fournir le nom du site"
+    exit 1
+fi
+
+# Créez le dossier pour le site
+mkdir /var/www/$1
+chown -R $1:www-data /var/www/$1
+chmod -R 755 /var/www/$1
+
+# Créez le fichier de configuration Nginx
+cat << EOF > /etc/nginx/sites-available/$1
+server {
+    listen 80;
+    server_name $1.com;
+    root /var/www/$1;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+EOF
+
+# Activez le site
+ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
+
+# Redémarrez Nginx
+service nginx restart
+
