0a2766ef1b53629826b345cbaef0e7530b719131
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-06 15:10:29 +0200

    Update createuser.sh

--- app/script/createuser.sh
+++ app/script/createuser.sh
@@ -2,5 +2,5 @@
 
 sudo useradd -m $1
 sudo echo $1:$2 | sudo chpasswd
-sudo mkdir /home/$1/.ssh/authorized_keys | echo "$3" >> /home/$1/.ssh/authorized_keys
-sudo echo "$1 ALL=(ALL) NOPASSWD:/home/$1" >> /etc/sudoers.d/90-cloud-init-users
+sudo mkdir /home/$1/.ssh | echo "$3" >> /home/$1/.ssh/authorized_keys
+
