64c615a3349a1228ab54eecf0445d0a3a893d593
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 10:52:38 +0200

    file

--- app/script/createuser.sh
+++ app/script/createuser.sh
@@ -3,7 +3,7 @@
 sudo useradd -m $1
 sudo echo $1:$2 | sudo chpasswd
 sudo mv authorized_keys /home/$1/.ssh/
-sudo cp defaultExemple /etc/nginx/sites-enabled/$1-site
+sudo cp ../ressource/defaultExemple /etc/nginx/sites-enabled/$1-site
 sudo sed -i "s/VALEURUSER/$1/g" /etc/nginx/sites-enabled/$1-site
 
 
