2a7b3800ec87eb15baa067bfa40b8925777e986b
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 14:27:24 +0200

    Update rightown.sh

--- app/script/rightown.sh
+++ app/script/rightown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # droit pour le repertoire du user
-sudo chown -R $1:www-data home/$1
+sudo chown -R $1:www-data /home/$1
 sudo chmod -R 755 /home/$1
 # droit pour le fichier ssh
-sudo chown -R $1:www-data home/$1/authorized_keys
-sudo chmod -R 755 /home/$1
+# sudo chown -R $1:www-data home/$1/authorized_keys
+# sudo chmod -R 755 /home/$1
