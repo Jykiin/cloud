87c54217bb1d5fac2b5aecc7e94f1d77b0e0e927
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 11:39:38 +0200

    fopen debug

--- app/script/create-user.php
+++ app/script/create-user.php
@@ -5,14 +5,15 @@ $password = $_GET['password'];
 $ssh = $_GET['ssh'];
 
 $file = fopen("authorized_keys", "w");
-fwrite($file, $ssh);
-fclose($file);
 var_dump($_GET);
 die;
+fwrite($file, $ssh);
+fclose($file);
+
 // shell_exec("./createuser.sh $username $password");
 
 // $bug = shell_exec("sudo mv authorized_keys /home/$username/.ssh/");
 
 echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
 
-shell_exec("./restartNginx.sh");
+// shell_exec("./restartNginx.sh");
