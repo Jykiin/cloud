18f8e3cd5f6478f25e24d7f1aa21e70250d99c51
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 15:16:05 +0200

    Update createuser.sh

--- app/script/createuser.sh
+++ app/script/createuser.sh
@@ -2,7 +2,8 @@
 
 sudo useradd -m $1
 sudo echo $1:$2 | sudo chpasswd
-sudo cat authorized_keys >> /home/$1/.ssh/authorized_keys | sudo rm authorized_keys
+sudo cat authorized_keys >> /home/$1/.ssh/authorized_keys 
+sudo rm authorized_keys
 # Créez le fichier de configuration Nginx
 sudo cp ../ressource/defaultExemple /etc/nginx/sites-enabled/$1-site
 sudo sed -i "s/VALEURUSER/$1/g" /etc/nginx/sites-enabled/$1-site
