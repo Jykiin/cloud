73c59fb19663ae39e394ec07241932cf5bb88716
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 15:50:11 +0200

    Update create-user.php

--- app/script/create-user.php
+++ app/script/create-user.php
@@ -18,7 +18,7 @@ fclose($file);
 echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
 fastcgi_finish_request();
 
-shell_exec("./restartNginx.sh");
+// shell_exec("./restartNginx.sh");
 
 $host = "localhost";
 $user = "root";
