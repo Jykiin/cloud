24ea7bfe234212aedb8d3e37523edee170a879cf
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-07 14:53:34 +0200

    hbjh

--- app/index.php
+++ app/index.php
@@ -10,10 +10,10 @@
     <div class="container w-100 flex align-items-center justify-content-center">
       <h1 class="mt-5 text-center fw-bold text-primary fs-4">Accueil projet cloud</h1>
       <p class="mt-2 text-center"> Bienvenue chez nous, inscrivez-vous ou rejoignez votre compte:</p>
-    <nav class="shadow border border-2 border-primary flex align-items-center justify-content-center">
-      <a class="my-2 btn btn-primary w-50" href="/">Accueil</a>
-      <a class="my-2 btn btn-primary w-50" href="src/sign-in.php">Inscription</a>
-      <a class="my-2 btn btn-primary w-50" href="src/sign-up.php">Connexion</a>
+    <nav class="shadow border border-1 border-success flex flex-column gap-2 align-items-center justify-content-center">
+      <a class="my-2 btn btn-primary w-25" href="/">Accueil</a>
+      <a class="my-2 btn btn-primary w-25" href="src/sign-in.php">Inscription</a>
+      <a class="my-2 btn btn-primary w-25" href="src/sign-up.php">Connexion</a>
   </nav>
 </div>
 
