0942b0e40f3f09cbf3e2375dc1bd42bf87346fa0
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 11:48:05 +0200

    Update create-user.php

--- app/script/create-user.php
+++ app/script/create-user.php
@@ -1,19 +1,15 @@
 <?php
-# Récupérer les données du formulaire HTML
+
 $username = $_GET['username'];
 $password = $_GET['password'];
 $ssh = $_GET['ssh'];
 
 $file = fopen("authorized_keys", "w");
-var_dump($file);
-die;
 fwrite($file, $ssh);
 fclose($file);
 
-// shell_exec("./createuser.sh $username $password");
-
-// $bug = shell_exec("sudo mv authorized_keys /home/$username/.ssh/");
+shell_exec("./createuser.sh $username $password");
 
 echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
 
-// shell_exec("./restartNginx.sh");
+shell_exec("./restartNginx.sh");
