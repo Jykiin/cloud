8407ab04530f6e2fcee4c1c575fdf144b756cb0e
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-25 13:20:31 +0200

    domaine name

--- app/script/createuser.sh
+++ app/script/createuser.sh
@@ -4,7 +4,7 @@ sudo useradd -m $1
 sudo echo $1:$2 | sudo chpasswd
 
 # Créer le fichier de configuration Nginx
-sudo cp ../ressource/defaultExemple /etc/nginx/sites-enabled/$3.groupe16.fr
-sudo sed -i "s/VALEURUSER/$1/g" /etc/nginx/sites-enabled/$3.groupe16.fr
+sudo cp ../ressource/defaultExemple /etc/nginx/sites-enabled/$3-groupe16.fr
+sudo sed -i "s/VALEURDOMAINE/$3/g" /etc/nginx/sites-enabled/$3-groupe16.fr
 # Créer une page d'accueil a l'utilisateur
 sudo sed -i "s/VALEURUSER/$1/g" /home/$1/www/index.html
--- app/src/sign-in.php
+++ app/src/sign-in.php
@@ -21,7 +21,7 @@
       </div>
         <div class="mb-3">
             <label for="domainName" class="form-label">Nom de domaine:</label>
-            <div><input  type="text" id="domainName" name="domainName" aria-describedby="domainName" required><span>.groupe16.fr</span></div>
+            <div><input  type="text" id="domainName" name="domainName" aria-describedby="domainName" required><span>-groupe16.fr</span></div>
         </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Mot de passe: </label>
