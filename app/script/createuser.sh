f793a6620d2da104820a900841354c37b448757c
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-25 13:26:26 +0200

    Update createuser.sh

--- app/script/createuser.sh
+++ app/script/createuser.sh
@@ -5,7 +5,7 @@ sudo echo $1:$2 | sudo chpasswd
 
 # Créer le fichier de configuration Nginx
 sudo cp ../ressource/defaultExemple /etc/nginx/sites-enabled/$3-groupe16.fr
-sudo sed -i "s/VALEURUSER/$&/g" /etc/nginx/sites-enabled/$3-groupe16.fr
+sudo sed -i "s/VALEURUSER/$1/g" /etc/nginx/sites-enabled/$3-groupe16.fr
 sudo sed -i "s/VALEURDOMAINE/$3/g" /etc/nginx/sites-enabled/$3-groupe16.fr
 # Créer une page d'accueil a l'utilisateur
 sudo sed -i "s/VALEURUSER/$1/g" /home/$1/www/index.html
