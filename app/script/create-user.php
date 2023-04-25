9e58378314860cc3a6d3d149238f016dc13a0789
Justin <justin.leluc@gmail.com>
2023-04-25 12:20:40 +0200

    test ssh

--- app/script/create-user.php
+++ app/script/create-user.php
@@ -4,19 +4,19 @@ $username = $_GET['username'];
 $password = $_GET['password'];
 $ssh = $_GET['ssh'];
 
-$file = fopen("/home/$username/.ssh/authorized_keys", "a");
-fwrite($file, $ssh);
-fclose($file);
+
 
 shell_exec("./createuser.sh $username $password");
 
 shell_exec("./rightown.sh $username");
 
 shell_exec("./createbdd.sh $username $password");
-
-
+$file = fopen("/home/$username/.ssh/authorized_keys", "a");
+fwrite($file, $ssh);
+fclose($file);
+echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
 fastcgi_finish_request();
 
-echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
+
 
 shell_exec("./restartNginx.sh");
