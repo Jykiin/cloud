fd4264ee64220b87f4ff426ee5fc1dbe77a07cc5
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-26 12:36:18 +0200

    controller:request method blocage added

--- app/script/login-user.php
+++ app/script/login-user.php
@@ -1,25 +1,50 @@
 <?php
-require dirname(__FILE__, 0).'getUsersData.php';
-require dirname(__FILE__, 2).'/src/connexion.php';
+require dirname(__FILE__, 0) . 'getUsersData.php';
+require dirname(__FILE__, 2) . '/src/connexion.php';
 
-if(isset($_POST['account_login']))  {
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
     $username = isset($_POST['username']) ? $_POST['username'] : null;
     $password = isset($_POST['password']) ? $_POST['password'] : null;
+    getConnected($username, $password);
 
-    if(isset($username) && isset($password)) {
-        $getUserData = new GetUserData("localhost", "groupe16", "", "groupe16");
+} elseif($_SERVER['REQUEST_METHOD'] === 'GET') {
+    header('Location: /../src/connexion.php');
+} else {
+    header('Location: /../src/connexion.php');
+}
+
+function getConnected($username,$password) {
+
+    $bdd_host = "localhost";
+    $bdd_username= "groupe16";
+    $bdd_password = "";
+    $bdd_name = "groupe16";
+
+    if (isset($username) && isset($password)) {
+        $getUserData = new GetUserData($bdd_host, $bdd_username, $bdd_password, $bdd_name);
         $userData = $getUserData->getByUserName($username);
-        var_dump($userData);
-        if($userData)  {
-            if($userData['username'] === $username && $userData['password'] === $password) {
+
+        if ($userData) {
+            if ($userData['username'] === $username && $userData['password'] === $password) {
                 $_SESSION['user'] = $username;
-                http_response_code(200);
+
                 header('Location: /src/welcome.php');
                 exit();
 
             }
+        } else {
+            // En cas d'erreur, redirection vers la page d'erreur
+            header('Location: /src/error_page/page-erreur.php');
+            exit();
         }
+    } else {
+        // En cas d'erreur, redirection vers la page d'erreur
+        header('Location: /src/error_page/page-erreur.php');
+        exit();
     }
 }
+
+
