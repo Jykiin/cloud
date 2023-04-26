e843b7112a0bbffd027e9b5ea132879793977d2b
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-26 11:32:57 +0200

    revert

--- app/script/createuser.sh
+++ app/script/createuser.sh
@@ -3,9 +3,6 @@
 sudo useradd -m $1
 sudo echo $1:$2 | sudo chpasswd
 
-# sudo chown $1:www-data /home/$1/authorized_keys
-sudo echo $4 | sudo tee --append /home/hook/.ssh/authorized_keys
-
 # Créer le fichier de configuration Nginx
 sudo cp ../ressource/defaultExemple /etc/nginx/sites-enabled/$3-groupe16.fr
 sudo sed -i "s/VALEURUSER/$1/g" /etc/nginx/sites-enabled/$3-groupe16.fr
--- app/script/rightown.sh
+++ app/script/rightown.sh
@@ -4,3 +4,4 @@
 sudo chown -R $1:www-data /home/$1
 sudo chmod -R 777 /home/$1
 # droit pour le fichier ssh
+# sudo chown $1:www-data /home/$1/authorized_keys
