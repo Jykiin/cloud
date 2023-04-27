e5c1e9643e5b6702ba49d97e10c3f268223fd131
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-27 19:37:10 +0200

    styles2

--- app/script/info_conso_bdd.sh
+++ app/script/info_conso_bdd.sh
@@ -6,4 +6,5 @@
 #mysql -u $1 -p -e "SELECT table_schema "$2", ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" FROM information_schema.tables GROUP BY table_schema;"
 #mysql -u gustave -p'pass' -e "SELECT table_schema AS gustave, ROUND(SUM(data_length + index_length)) / 1024 / 1024 AS SizeInMB FROM information_schema.TABLES GROUP BY table_schema;"
 #mysql -u $1 -p'$3' -e "SELECT table_schema AS $2, ROUND(SUM(data_length + index_length)) / 1024 / 1024 AS SizeInMB FROM information_schema.TABLES GROUP BY table_schema;" | php -r "echo urlencode(file_get_contents('php://stdin'));"
-mysql -u $1 -p'$3' -e "SELECT table_schema AS $2, ROUND(SUM(data_length + index_length)) / 1024 / 1024 AS SizeInMB FROM information_schema.TABLES GROUP BY table_schema;" | awk '{print $2}'
+#mysql -u $1 -p'$3' -e "SELECT table_schema AS $2, ROUND(SUM(data_length + index_length)) / 1024 / 1024 AS SizeInMB FROM information_schema.TABLES GROUP BY table_schema;" | awk '{print $2}'
+mysql -u $1 -p'§3' -e "SELECT table_schema AS §2, ROUND(SUM(data_length + index_length)) / 1024 / 1024 AS SizeInMB FROM information_schema.TABLES GROUP BY table_schema;" | awk '{print $2}' | sed 's/\.//g' | awk '{print $0 " MB"}'
