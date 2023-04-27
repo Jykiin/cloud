966fcd405fbdcd79fd87eb7c215c44325886c707
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-27 15:33:58 +0200

    dnskjf

--- app/index.php
+++ app/index.php
@@ -13,7 +13,7 @@ echo $username;
     <title>Accueil</title>
   </head>
   <body>
-    <div class="container w-100 d-flex align-items-center justify-content-center">
+    <div class="container w-100 d-flex flex-column align-items-center justify-content-center">
       <h1 class="mt-5 text-center fw-bold text-primary fs-4">Accueil projet cloud</h1>
       <p class="mt-2 text-center"> Bienvenue chez nous, inscrivez-vous ou rejoignez votre compte:</p>
       <nav class="shadow border border-1 border-success d-flex flex-column gap-2 align-items-center justify-content-center">
