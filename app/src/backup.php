d602f970d4e3b29815b780764aa1f92619b07ac1
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-27 16:05:59 +0200

    Update backup.php

--- app/src/backup.php
+++ app/src/backup.php
@@ -9,7 +9,7 @@ $backup_files = array_diff(scandir($BACKUP_DIR), array('..', '.'));
 
 // Affichage de la liste backups
 foreach ($backup_files as $file) {
-    if (strpos($file, "marco") !== false) {
+    if (strpos($file, $_SESSION['username']) !== false) {
         echo "<a href='/src/download.php?file=$file'>$file</a><br>";
     }
 }
