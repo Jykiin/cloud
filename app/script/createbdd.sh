f9f94481ac7bb03b1d3aea2fb5bda60b142bdb7d
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-28 01:23:14 +0200

    Update createbdd.sh

--- app/script/createbdd.sh
+++ app/script/createbdd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo mysql -e "CREATE DATABASE $1;"
+sudo mysql -e "CREATE DATABASE '$1';"
 sudo mysql -e "CREATE USER '$1'@'localhost' IDENTIFIED BY '$2';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost';"
