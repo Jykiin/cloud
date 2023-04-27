3319bc49218e78dba0099c5bd5942f12a842263b
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-27 18:20:51 +0200

    styles2

--- app/script/infoConsoSite.php
+++ app/script/infoConsoSite.php
@@ -1,11 +1,12 @@
 <?php
+session_start();
 require dirname(__FILE__, 0) . 'getUsersData.php';
-$username = 'gustave';
 
-//$bdd_host = "localhost";
-//$bdd_username= "groupe16";
-//$bdd_password = "";
-//$bdd_name = "groupe16";
+$username = $_SESSION['username'];
+$bdd_host = "localhost";
+$bdd_username= "groupe16";
+$bdd_password = "";
+$bdd_name = "groupe16";
 
 $username_folder = null;
 $user_bdd_name = null;
@@ -13,23 +14,28 @@ $user_bdd_name = null;
 $userSiteSize = "";
 $userBddSize = "";
 
-//if (isset($username)) {
-//    $getUserData = new GetUserData($bdd_host, $bdd_username, $bdd_password, $bdd_name);
-//    $userData = $getUserData->getByUserName($username);
-//
-//    $username_folder = $userData['username'];
-//    $user_bdd_name = $userData['username'];
-//}
+if (isset($username)) {
+    $getUserData = new GetUserData($bdd_host, $bdd_username, $bdd_password, $bdd_name);
+    $userData = $getUserData->getByUserName($username);
 
+    $username_folder = $userData['username'];
+    $user_bdd_name = $userData['username'];
+}
 
 # récupération de la taille du dossier utilisateur
-$username_folder = 'gustave';
+if(!empty($username_folder))  {
 $dossier_utilisateur = shell_exec("./info_conso_site.sh $username_folder");
-
-//# récupération de la taille de la base de données utilisateur
-//$db_utilisateur = shell_exec("./info_conso_bdd.sh $username $user_bdd_name");
-$userBddSize = "lapin";
-$userSiteSize = $dossier_utilisateur;
+    $userSiteSize = $dossier_utilisateur;
+} else {
+    header("Location: /?error_data=invalid_folder_values");
+}
+# récupération de la taille de la base de données utilisateur
+if(!empty($username_folder) && !empty($user_bdd_name))  {
+$db_utilisateur = shell_exec("./info_conso_bdd.sh $username $user_bdd_name");
+    $userBddSize = $db_utilisateur;
+} else {
+    header("Location: /?error_data=invalid_bdd_values");
+}
 
 if(!empty($username_folder))  {
     header("Location: /?user_site_size=" . urlencode($userSiteSize)."&user_bdd_size=". urlencode($userBddSize));
