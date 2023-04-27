6f1e8390ffd16d6ba2b23b7634f300394275589f
Justin leluc <justin.leluc@gmail.com>
2023-04-27 12:03:31 +0200

    Create second website

--- app/script/create-site.sh
+++ app/script/create-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Créer le fichier de configuration Nginx
-sudo cp ../ressource/defaultExemplesite2 /etc/nginx/sites-enabled/$3-groupe16.fr
-sudo sed -i "s/VALEURUSER/$1/g" /etc/nginx/sites-enabled/$3-groupe16.fr
-sudo sed -i "s/VALEURDOMAINE/$3/g" /etc/nginx/sites-enabled/$3-groupe16.fr
+sudo cp ../ressource/defaultExemplesite2 /etc/nginx/sites-enabled/$2-groupe16.fr
+sudo sed -i "s/VALEURUSER/$1/g" /etc/nginx/sites-enabled/$2-groupe16.fr
+sudo sed -i "s/VALEURDOMAINE/$2/g" /etc/nginx/sites-enabled/$2-groupe16.fr
 # Créer une page d'accueil a l'utilisateur
 sudo mkdir /home/$1/www2
 sudo cp ../ressource/index.html /home/$1/www2/index.html
