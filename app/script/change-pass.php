2ee0516d0d6f8e6009c8f1cea6c5abdf0ed0365d
reewaz401 <reewazmaskey@gmail.com>
2023-04-27 15:15:32 +0200

    change pass

--- app/script/change-pass.php
+++ app/script/change-pass.php
@@ -10,9 +10,25 @@
       if($newpassword !== $confirmpassword) {
           echo "Error: New password and confirm password do not match.";
       } else {
+        $mysqli = new mysqli("localhost","groupe16","","groupe16");
+        // Check connection
+        if ($mysqli -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+            exit();
+        }
+        $sql = "UPDATE users SET pwd = '$newpassword' WHERE username = '$username';";
+        if ($mysqli->query($sql)) {
+            echo("Password updated successfully.<br />");
+            header('Location: /');
+        }
+        if ($mysqli->errno) {
+            echo("Could not update password <br />".$mysqli->error);
+        }
+        $mysqli->close();
+        // fastcgi_finish_request();
+        // shell_exec("./restartNginx.sh");
           // Use the exec function to run the passwd command
-          $command = "echo '$newpassword\n$newpassword' | passwd --stdin $username";
-          shell_exec("echo '$oldpassword' | sudo -S $command 2>&1", $output, $return_var);
+          shell_exec("./changePass.sh",$username,$newpassword);
 
           // Check the return status and display a message accordingly
           if($return_var === 0) {
---
+++ app/script/changePass.sh
@@ -0,0 +1,1 @@
+sudo echo "$1:$2" | sudo chpasswd
