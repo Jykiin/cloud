ecf883c19610334b7bf5a8a02b9a31c5d9017352
Amaury Franssen <amauryfranssen@gmail.com>
2023-04-28 03:07:06 +0200

    new change

--- app/index.php
+++ app/index.php
@@ -40,7 +40,6 @@ require_once __DIR__. '/script/getUsersData.php';
                       <?php
                       $username = $_SESSION['username'];
                       $getUserData = new GetUserData('localhost', 'groupe16', '', 'groupe16');
-                      $userData = $getUserData->getDomainsByUserName($username);
                       $domains = $getUserData->getDomainsByUserName($username);
                           echo '<h4> Site(s) web sur mon compte: </h4>';
                           echo '<p class="my-2 text-info text-center fw-bold">Cliquez sur le ou les site(s) web pour obtenir vos données de consommations.</p>';
