e385babef54a842fd4874a5e49800a7626e9c4f3
reewaz401 <reewazmaskey@gmail.com>
2023-04-28 10:21:57 +0200

    var

--- app/script/bonus.php
+++ app/script/bonus.php
@@ -3,18 +3,21 @@ function spaceUsed($username){
 // session_start();
 $command = "sudo du -s /home/{$username} | awk '{print $1}'";
 $output = shell_exec($command);
+var_dump($output);
 return formatBytes($totalUsedSpace);
 }
 
 function memoryUsed(){
 $command = "cat /proc/meminfo | grep '^Uid' | awk '{user=int($2); getline < \"/proc/stat\"; mem=$6; printf \"User %d Memory: %s kB\n\", user, mem}'";
 $output = shell_exec($command);
+var_dump($output);
 return $output;
 
 }
 function cpuLoad(){
 $command = "cat /proc/stat | grep '^cpu ' | awk '{total=$2+$3+$4+$5; user=$2; system=$4; usage=(user+system)*100/total} END {printf \"User CPU Load: %.2f%%\\n\", usage}'";
 $output = shell_exec($command);
+var_dump($output);
 return $output;
 }
 
